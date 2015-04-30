eaa10b47738ed3eb5a4ff8620038c9e5889a7bef
maximburyak <maxim@ayende.com>
2015-04-30 17:10:14 +0300

    Basic draft

---
+++ src/main/java/Console.java
@@ -0,0 +1,39 @@
+/**
+ * Created by HR on 4/30/2015.
+ */
+public class Console {
+    public static void main(String[] args){
+
+        int input = 0;
+
+        while(input  != -1) {
+            System.out.println("Choose Your Demonstration:");
+            System.out.println("1) Transactions");
+            System.out.println("2) Replication Failover");
+            System.out.println("3) Bulk Insert");
+            System.out.println("4) Lazy Queries");
+            System.out.println("5) Changes API");
+            System.out.println("6) Aggressive Caching");
+            System.out.println("7) Transformers (With Parameters) - mauro serviente");
+            System.out.println("8) Streaming - demonstrations with fiddler and continuous query");
+
+            System.out.println("20) Clear Screen");
+
+
+            /*
+            * WOW Features:
+            * 1) Query Streaming
+            * 2) Full Text Search (also, define indexes)
+            * 3) Map Reduce
+            * 4) Multi Map Reduce
+            * 5) Map Reduce visualizer
+            * 6) 
+            * */
+            System.out.println("Choose Your Demonstration:");
+        }
+    }
+
+    public void ProcessCommand(String command){
+
+    }
+}
