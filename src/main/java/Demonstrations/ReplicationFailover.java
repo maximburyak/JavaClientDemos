b911b4bd0e53e7ddfeeb5e2da532f94ed675e32c
maximburyak <maxim@ayende.com>
2015-05-06 11:13:14 +0300

    basic replication filover

--- src/main/java/Demonstrations/ReplicationFailover.java
+++ src/main/java/Demonstrations/ReplicationFailover.java
@@ -1,7 +1,32 @@
 package Demonstrations;
 
+import net.ravendb.client.IDocumentSession;
+import net.ravendb.client.document.DocumentQueryCustomizationFactory;
+import net.ravendb.client.document.DocumentStore;
+import net.ravendb.client.document.FailoverBehavior;
+import net.ravendb.client.document.FailoverBehaviorSet;
+import org.apache.commons.lang.time.StopWatch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 /**
  * Created by bumax_000 on 5/3/2015.
  */
 public class ReplicationFailover {
+
+    public static void InsertDataWithReplication(){
+        try(DocumentStore store = new DocumentStore("http://localhost:8080", "MyNewDB")){
+
+            // initialization
+            store.getConventions().
+                    setFailoverBehavior(
+                            FailoverBehaviorSet.of(
+                                    FailoverBehavior.ALLOW_READS_FROM_SECONDARIES_AND_WRITES_TO_SECONDARIES));
+            store.initialize();
+        }
+
+    }
 }
