6a44a55402fd19659a5ab028efdbcbf61f18727f
maximburyak <maxim@ayende.com>
2015-11-04 10:32:31 +0100

    added simple query code example

--- src/main/java/Demonstrations/QueryAPIDemo.java
+++ src/main/java/Demonstrations/QueryAPIDemo.java
@@ -2,40 +2,35 @@ package Demonstrations;
 
 import TransactionalBank.Account;
 import TransactionalBank.QAccount;
-import net.ravendb.abstractions.LinqOps;
 import net.ravendb.abstractions.basic.CloseableIterator;
-import net.ravendb.abstractions.data.Constants;
 import net.ravendb.abstractions.data.StreamResult;
-import net.ravendb.client.IDocumentQuery;
 import net.ravendb.client.IDocumentSession;
 import net.ravendb.client.IDocumentStore;
 import net.ravendb.client.document.DocumentStore;
 import net.ravendb.client.linq.IRavenQueryable;
 
+import java.util.List;
+
 /**
  * Created by bumax_000 on 5/6/2015.
  */
-public class StreamingAPIDemo extends DemonstrationBase {
+public class QueryAPIDemo extends DemonstrationBase {
 
     IDocumentStore store;
-    public StreamingAPIDemo(){
+    public QueryAPIDemo(){
         store = new DocumentStore("http://localhost:8080", "TreasuryDB").initialize();
         //useFiddler(store);
     }
 
     @Override
     public void execute(){
-        System.out.println("Press any key to start printing all accounts");
 
         try(IDocumentSession session = store.openSession()){
             QAccount account = QAccount.account;
-            IRavenQueryable accountsQuery = session.query(Account.class)
-                    .where(account.owner.eq("foo"));
-            CloseableIterator<StreamResult> accountsIteration = session.advanced().stream(accountsQuery);
-            while(accountsIteration.hasNext()){
-                Account curAccount = (Account)accountsIteration.next().getDocument();
-                System.out.println(String.format("Owner: %1$s Balance: %2$s", curAccount.getOwner(), curAccount.getBalance()));
-            }
+            Account johnAcc = session.query(Account.class)
+                    .first(account.owner.eq("John"));
+
+            // do something with this account
         }
     }
 
